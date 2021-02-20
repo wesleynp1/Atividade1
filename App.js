094b7f295ac1c683ff82138e3406f4362b3b5683
Wesley <wesleynp987@gmail.com>
2021-02-20 11:49:11 -0300

    Efetuada correções de sintaxe

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ export default function App() {
       <View style={styles.results}>
         <Text style={styles.historyText}>{lastNumber}</Text>
         <Text style={styles.resultText}>{currentNumber}</Text>
-      <View>
+      </View>
 
       {/* Area onde os botões são exibidos*/}
       <View style={styles.buttons}>
